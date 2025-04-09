8f2a4ffd442d8aeb568dc630317abdf7450399ea
vic-dt <victoriavr1323@gmail.com>
2025-04-09 17:55:07 +0200

    Botón personalizado con slot

--- src/modulorum/projects/views/ProjectsView.vue
+++ src/modulorum/projects/views/ProjectsView.vue
@@ -25,12 +25,15 @@
 
 </div>
 
-<FabButton />
+<fab-button @click="()=> console.log('clicked')">
+  <AddCircle/>
+</fab-button> 
 
 </template>
 
 <script lang="ts" setup>
 import FabButton from '@/modulorum/commune/components/FabButton.vue';
+import AddCircle from '@/modulorum/commune/icons/AddCircle.vue';
 
 
 </script>
