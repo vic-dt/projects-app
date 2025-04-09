6364d4bbbd0174335f24e307821416354d679503
vic-dt <victoriavr1323@gmail.com>
2025-04-09 19:49:21 +0200

    Input personalizado

--- src/modulorum/commune/components/InputModal.vue
+++ src/modulorum/commune/components/InputModal.vue
@@ -1,19 +1,20 @@
 <template>
-  <dialog id="my_modal_1" class="modal" :open="true">
+  <dialog id="my_modal_1" class="modal" :open="aperta">
     <div class="modal-box">
-      <h3 class="text-lg font-bold">Hello!</h3>
-      <p class="py-4">Press ESC key or click the button below to close</p>
+      <h3 class="text-lg font-bold">{{ titulus }}</h3>
+      <p class="py-4">{{ subtitulus }}</p>
       <div class="modal-action flex flex-col">
         <form method="dialog" @submit.prevent="submitValorem">
           <input
             type="text"
-            placeholder="Nombre del Proyecto"
+            :placeholder="placeholder ?? 'Inserte un valor'"
             class="input input-bordered w-full flex input-primary"
             v-model="inputValorem"
+            ref="inputRef"
           />
 
           <div class="flex justify-end mt-5">
-            <button class="btn">Close</button>
+            <button @click="$emit('claudere')" class="btn">Close</button>
             <button type="submit" class="btn btn-primary ml-4">Aceptar</button>
           </div>
         </form>
@@ -21,7 +22,7 @@
     </div>
   </dialog>
 
-  <div class="fixed top-0 w-screen h-screen z-10 bg-black opacity-40"></div>
+  <div v-if="aperta" class="fixed top-0 w-screen h-screen z-10 bg-black opacity-40"></div>
 </template>
 
 <script lang="ts" setup>
@@ -29,19 +30,26 @@ import { ref } from 'vue';
 
 interface Props {
   aperta: boolean;
+  placeholder?: string;
+  titulus: string;
+  subtitulus: string;
 }
+defineProps<Props>();
 
 const emissiones = defineEmits<{
   claudere: [void];
   valorem: [text: string];
 }>();
 
-const inputValorem = ref('');
+const inputValorem = ref<string>('');
+
+const inputRef = ref<HTMLInputElement | null>(null);
 
 const submitValorem = () => {
   console.log({ valorem: inputValorem.value });
 
   if (!inputValorem.value) {
+    inputRef.value?.focus()
     return;
   }
 
--- src/modulorum/projects/views/ProjectsView.vue
+++ src/modulorum/projects/views/ProjectsView.vue
@@ -25,9 +25,16 @@
 
 </div>
 
-<InputModal/>
+<InputModal 
+:aperta="modalAperta" 
+@claudere="modalAperta=false"
+@valorem="cumNovusValorem"
+placeholder="Introduce el nombre del proyecto"
+titulus="Nuevo proyecto"
+subtitulus="Dale un nombre apropiado a tu proyecto"
+/>
 
-<fab-button @click="()=> console.log('clicked')">
+<fab-button @click="modalAperta=true">
   <AddCircle/>
 </fab-button> 
 
@@ -37,6 +44,13 @@
 import FabButton from '@/modulorum/commune/components/FabButton.vue';
 import InputModal from '@/modulorum/commune/components/InputModal.vue';
 import AddCircle from '@/modulorum/commune/icons/AddCircle.vue';
+import { ref } from 'vue';
+
+const modalAperta = ref(false);
+
+const cumNovusValorem = (projectNomen: string)=> {
+  console.log(projectNomen)
+}
 
 
 </script>
