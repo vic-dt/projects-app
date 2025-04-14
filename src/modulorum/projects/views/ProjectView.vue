fb146a6d9565841cef140c5972cfcec828d5ec2d
vic-dt <victoriavr1323@gmail.com>
2025-04-14 19:56:50 +0200

    Breadcrumbs

---
+++ src/modulorum/projects/views/ProjectView.vue
@@ -0,0 +1,31 @@
+<template>
+    <BreadCrumbs :nomen="project?.nomen ?? 'No name'" />
+</template>
+
+<script setup lang="ts">
+import BreadCrumbs from '@/modulorum/commune/components/BreadCrumbs.vue';
+import { useProjectsStore } from '../stores/projects.store';
+import { ref, watch } from 'vue';
+import type { Project } from '../Interfaces/project.interface';
+
+
+interface Props {
+    id: string;
+
+}
+
+ const props = defineProps<Props>();
+
+ const projectStore = useProjectsStore();
+
+const project = ref<Project | null>()
+
+
+ watch(()=> props.id, () =>{
+    project.value = projectStore.projectList.find(project => project.id === props.id)
+ },
+ {
+    immediate: true
+ });
+</script>
+
